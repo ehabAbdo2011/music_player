254620cee34b7f8d1836024fd355da039c295b67
ehabAbdo2011 <ehabAbdo2011@users.noreply.github.com>
2018-07-10 00:46:27 +0200

    music player

--- app/src/main/java/com/example/ehab2/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/ehab2/musicplayer/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
@@ -15,21 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mediaPlayer = MediaPlayer.create(this, R.raw.sound);
-        Button play_button = findViewById(R.id.play_Button);
+        mediaPlayer = MediaPlayer.create(this, R.raw.cccc);
+        ImageButton play_button = findViewById(R.id.play_Button);
         play_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mediaPlayer.start();
-
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        Toast.makeText(MainActivity.this,"Done",Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
-        Button pause_button = findViewById(R.id.pause_Button);
+        ImageButton pause_button = findViewById(R.id.pause_Button);
         pause_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mediaPlayer.pause();
             }
         });
+
     }
 }
